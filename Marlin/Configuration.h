7ea17805342cb50e5fa9c4592eb5f5fa4467d7f5
pixelsafoison <64530907+pixelsafoison@users.noreply.github.com>
2020-09-06 19:53:22 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Baphomet"
+#define CUSTOM_MACHINE_NAME "Rosie"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
