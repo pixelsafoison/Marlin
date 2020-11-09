822029e50cf7255526e4a1f2b7a825b77344ab8b
pixelsafoison <64530907+pixelsafoison@users.noreply.github.com>
2020-11-09 13:24:00 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1077,9 +1077,9 @@
 
   // Add Probe Z Offset calibration to the Z Probe Offsets menu
   #if HAS_BED_PROBE
-    //#define PROBE_OFFSET_WIZARD
+    #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START -4.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -1.0   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
